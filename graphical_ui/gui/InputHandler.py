e3188b73972f05ec95bb5abe194c4d6fd94067c5
Ryan Jacobson <jacobsonryan28@gmail.com>
2019-11-07 10:32:48 -0700

    Created very basic fram of GUI

--- graphical_ui/gui/InputHandler.py
+++
@@ -1,4 +0,0 @@
-# This Python file uses the following encoding: utf-8
-from PyQt5 import QtCore
-from PyQt5 import QtWidgets
-from MainWindow import Ui_MainWindow
---
+++ graphical_ui/src/main.py
@@ -0,0 +1,32 @@
+import sys
+import os
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QFileDialog
+from Ui_main_window import Ui_GUI
+
+
+class GuiMainWindow(QtWidgets.QMainWindow, Ui_GUI):
+    """Class to setup GUI widgets and connect them."""
+    def __init__(self):
+        super(GuiMainWindow, self).__init__()
+        self.setupUi(self)
+        self.mainStackedWidget.setCurrentIndex(0)
+        self.fileSelectButton.clicked.connect(self.select_file)
+        self.submitButton.clicked.connect(self.file_submitted)
+    def select_file(self):
+        """Open file dialog when file select is pressed."""
+        self.filePathLineEdit.setText(QFileDialog.getOpenFileName()[0])
+        filename, file_extension = os.path.splitext(self.filePathLineEdit.text())
+        if file_extension == ".bpmn":
+            self.missionName.setText(os.path.basename(filename))
+        else:
+            self.filePathLineEdit.clear()
+    def file_submitted(self):
+        """Switch to main page when a file submitted."""
+        self.mainStackedWidget.setCurrentIndex(1)
+
+
+APP = QtWidgets.QApplication(sys.argv)
+WINDOW = GuiMainWindow()
+WINDOW.show()
+APP.exec_()
